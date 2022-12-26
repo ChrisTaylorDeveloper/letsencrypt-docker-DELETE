4ca6eefc6cf76c568fb5efcfff646f6f21242545
Chris Taylor <chris@christaylordeveloper.co.uk>
2022-12-26 16:42:15 +0000

    temp

--- run.sh
+++ run.sh
@@ -2,5 +2,7 @@ docker-compose down
 docker system prune -a --force
 docker volume rm $(docker volume ls -q)
 docker-compose up --build -d
+echo "certbot logs"
 docker-compose logs certbot
+echo "nginx logs"
 docker-compose logs nginx
