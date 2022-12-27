b1be0d0761beace4c53e610d9b5a5dc79de8e5de
Chris Taylor <chris@christaylordeveloper.co.uk>
2022-12-27 17:01:40 +0000

    temp

--- run.sh
+++ run.sh
@@ -1,8 +1,16 @@
+#!/bin/bash
+
 docker-compose down
-docker system prune -a --force
-docker volume rm $(docker volume ls -q)
-docker-compose up --build -d
-echo "certbot logs"
-docker-compose logs certbot
-echo "nginx logs"
-docker-compose logs nginx
+
+if [[ $RESET_HARD == "1" ]]
+    then echo "True"
+    else echo "False"
+fi
+
+# docker system prune -a --force
+# docker volume rm $(docker volume ls -q)
+# docker-compose up --build -d
+# echo "certbot logs"
+# docker-compose logs certbot
+# echo "nginx logs"
+# docker-compose logs nginx
