37028546a070254d147d29284bb8f0b88033c6dd
Chris Taylor <chris@christaylordeveloper.co.uk>
2022-12-27 17:06:27 +0000

    temp

--- run.sh
+++ run.sh
@@ -3,8 +3,8 @@
 docker-compose down
 
 if [[ $RESET_HARD == "1" ]]
-    docker system prune -a --force
-    docker volume rm $(docker volume ls -q)
+    docker system prune -a --force;
+    docker volume rm $(docker volume ls -q);
 fi
 
 # docker-compose up --build -d
